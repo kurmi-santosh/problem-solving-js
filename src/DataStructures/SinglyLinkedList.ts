211fcecd64d1582472dc83526558824d9c21a86a
Kurmi <kurmi.gorle@cerner.com>
2024-02-13 14:35:51 +0530

    Shift, Unshift, Insert and Remove - SinglyLinkedList

--- src/DataStructures/SinglyLinkedList.ts
+++ src/DataStructures/SinglyLinkedList.ts
@@ -49,6 +49,72 @@ export class SinglyLinkedList {
         }
         return currentNode;
     }
+
+    shift(){
+        if(!this.head) return undefined;
+        let firstNode = this.head;
+        // We have to remove this firstNode (current head). 
+        // Make firstNode's next as new head
+        this.head = firstNode.next;
+        this.length--;
+        return firstNode;
+
+    }
+
+    unshift(val: any){
+        let newNode = new SimpleNode(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index:number){
+        if(index < 0 || index > this.length) return undefined;
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter!== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+
+    insert(index:number, val: any){
+        if(index < 0 || index > this.length) return undefined;
+        if(index == 0) this.unshift(val);
+        else if(index == this.length) this.push(val);
+        else {
+            let newNode = new SimpleNode(val);
+            let prevNode = this.get(index-1);
+            
+            let temp = prevNode.next;
+            prevNode.next = newNode;
+            newNode.next = temp;
+        }
+        this.length++;
+        return true;
+    }
+
+    remove(index:number){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index == 0) this.shift()
+        else if(index == this.length-1) this.pop();
+        else {
+            let prevNode = this.get(index-1);
+            var removedNode = prevNode.next;
+            prevNode.next = removedNode.next;
+        }
+        this.length--;
+        return removedNode;
+    }
 }
 
 
--- src/DataStructures/SinglyLinkedListReadme.md
+++ src/DataStructures/SinglyLinkedListReadme.md
@@ -55,6 +55,48 @@
 - If there are no nodes, return undefined
 - We need to pop the tail. So set the 2nd last node's next prop to null.
   - (Starting from head, We need to use a while loop checking for next prop to be null)
-- set the 2nd last node as tail
+- Set the 2nd last node as tail
 - Decrement length by 1.
 - If the length is 0 after removing the node, make head and tail to be null;
+
+### Implement Shifting
+
+- We need to remove existing head and make it's next node as new head
+- If there are no nodes, return undefined
+- Store the current head property in as firstNode variable
+- Set the head property to be the firstNode's next
+- Decrement the length by 1
+- return the firstNode
+
+### Implement unshift
+
+- We need to insert new node before head and make this as new head
+- Should accept a value - and Create new node
+- If there are no nodes, set the head and tail to be this new node
+- Otherwise set this new nodes next to current head
+- Set this new node as head
+- Increment length by 1
+- Return the linked list
+
+### Implement insert
+
+- where should we insert ? should receive the index value.
+- If index > length or index < 0 - return false
+- If the index is 0 - unshift(val)
+- If the index is length - push(val)
+- Otherwise GET the node at the (index-1)
+  - We need to name this as prevNode and this prevNode.next should be preserved at temp.
+  - Set the next prop of this prevNode to be our newNode
+  - Set the next prop of our newNode to be the preserved value (temp)
+
+### Implement remove
+
+- Should accept index
+- If index >= length or index < 0 return undefined
+- If index is 0 - shift()
+- If index = length-1 - pop()
+- Otherwise GET the node at (index-1)
+  - Preserve this prevNode.next as removedNode
+  - Set the prevNode.next to be removedNode.next
+  - decrement the length
+  - return the removedNode
